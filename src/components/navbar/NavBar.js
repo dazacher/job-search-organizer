347df44c2a65a652e805410234398c93d1ca9e27
dazacher <dazacher2000@yahoo.com>
2020-10-19 18:55:29 -0700

    Changed navbar to have a header above it.

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import { Nav, Image, Col, Row } from "react-bootstrap";
+import { Nav, Navbar, Image, Col, Row } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import UserImage from "../../assets/images/Me.jpeg";
 import JobSearchImage from "../../assets/images/Job_Search_Image.png";
@@ -15,67 +14,77 @@ import "./NavBar.css"
 const NavbarRx = () => {
   return (
     <>
-    <Navbar className="navbar" responisve="true" variant="dark" expand="lg">
-
-      <NavLink to="/">
-        <Navbar.Brand>
-          <Row>
-            <Col xs={12} md={9} lg={3}>
-              <Image
-                src={UserImage}
-                width="125px"
-                height="150px"
-                className="img-responsive mx-auto"
-                roundedCircle
-                id="deeannimg"
-                alt="User Image"
-              />
-            </Col>
-            <Col>
-              <h1 className="align-top inline">Dee Ann Scanniello</h1>
-              <Row>
-                <Col>
-                  <h4 className="inline">1901 W. Madison Street, #419</h4>
-                  <h4 className="inline"> Phoenix, AZ  85009</h4>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Navbar.Brand>
-      </NavLink>
-      <div className="jobSearch">
+      <div className="siteHeader">
         <Row>
-    <Col  xs={12} md={9} lg={3}>
-      <Image
-                src={JobSearchImage}
-                width="225px"
-                height="200px"
-                className="img-responsive2"
-                id="jobsearch"
-                alt="User Image"
-              />
-      
-      </Col>
-      </Row>
-      </div>
-    </Navbar >
-    <Navbar className="navbar2">
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav classsName="navbar2">
-          <NavLink to="/" exact className="nav-link">
-            Home
+          <Col xs={12} md={9} lg="auto" className="responsive">
+            <Image
+              src={UserImage}
+              width="125px"
+              height="150px"
+              className="img-responsive mx-auto"
+              roundedCircle
+              id="deeannimg"
+              alt="User Image"
+            />
+          </Col>
+          <Col xs={12} md={9} lg="auto" className="responsive row1">
+            <div>
+            <Row>
+              <Col>
+                <h3 className="align-top">Dee Ann Scanniello</h3>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={9} lg="auto">
+                <h5>1901 W. Madison Street, #419</h5>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={9} lg="auto">
+                <h5> Phoenix, AZ  85009</h5>
+              </Col>
+            </Row>
+            </div>
+          </Col>
+          <Col xs={12} md={9} lg="auto" className="jobSearch">
+            <Row>
+              <Col xs={12} md={9} lg="auto">
+                <Image
+                  src={JobSearchImage}
+                  width="225px"
+                  height="200px"
+                  className="img-responsive2"
+                  id="jobsearch"
+                  alt="User Image"
+                />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div >
+      <Navbar className="navbar" responisve="true" variant="dark" expand="lg">
+
+        <NavLink to="/">
+          <Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav classsName="navbar2">
+                <NavLink to="/" exact className="nav-link">
+                  Home
+                  </NavLink>
+                <NavLink to="/jobs" className="nav-link">
+                  Jobs
+                 </NavLink>
+                <NavLink to="/userData" className="nav-link">
+                  User
+                  </NavLink>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar.Brand>
         </NavLink>
-          <NavLink to="/jobs" className="nav-link">
-            Jobs
-        </NavLink>
-          <NavLink to="/userData" className="nav-link">
-            User
-        </NavLink>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+      </Navbar>
+
     </>
   );
 };
